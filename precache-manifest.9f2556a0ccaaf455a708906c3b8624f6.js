b2558c677092a9403b83391ba76f4ef974e29317
Adam Cooper <frankidyfrank3@gmail.com>
2019-10-24 10:48:57 -0500

    Updates

---
+++ precache-manifest.9f2556a0ccaaf455a708906c3b8624f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b6c6b3b480637046b3ff633bf8e91f1",
+    "url": "/static/media/logo.8b6c6b3b.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "55a609e3920a72450583",
+    "url": "/static/js/main.2313b1df.chunk.js"
+  },
+  {
+    "revision": "09eab75be627ae4b5cf7",
+    "url": "/static/js/2.cd10fab4.chunk.js"
+  },
+  {
+    "revision": "55a609e3920a72450583",
+    "url": "/static/css/main.2a985d3a.chunk.css"
+  },
+  {
+    "revision": "f16cc292838f671856c668f6b264e93c",
+    "url": "/index.html"
+  }
+];
